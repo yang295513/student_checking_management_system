4b3cb42e631bb35ad7da67b30716501016d407f9
yang295513 <15993343299@163.com>
2019-12-03 21:38:43 +0800

    以一次提交搭建了这个查重项目的基本架构

---
+++ src/main/java/com/qs304/student_checking_management_system/StudentCheckingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.qs304.student_checking_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCheckingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCheckingManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qs304/student_checking_management_system/StudentCheckingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qs304.student_checking_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCheckingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
