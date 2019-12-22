d6af88604e84afd262f4ed0e169bb780126359c8
yang295513 <15993343299@163.com>
2019-12-23 07:40:58 +0800

    只可意会不可言传的更新

--- src/main/java/com/qs304/student_checking_management_system/config/ConfigurerAdapter.java
+++ src/main/java/com/qs304/student_checking_management_system/config/ConfigurerAdapter.java
@@ -2,10 +2,12 @@ package com.qs304.student_checking_management_system.config;
 
 import com.qs304.student_checking_management_system.Adapter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableRedisHttpSession
 public class ConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
